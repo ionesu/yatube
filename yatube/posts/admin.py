3a22e348c9f73143e26be4c9d7f5295a5069d861
Ivan Sushkov <ivan.sushkov91@gmail.com>
2020-09-23 14:20:31 +0300

    Add: Template dir and index.html

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+# из файла models импортируем модель Post
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    list_display = ("pk", "text", "pub_date", "author") 
+    # добавляем интерфейс для поиска по тексту постов
+    search_fields = ("text",) 
+    # добавляем возможность фильтрации по дате
+    list_filter = ("pub_date",) 
+    
+    empty_value_display = "-пусто-"
+
+# при регистрации модели Post источником конфигурации для неё назначаем класс PostAdmin
+admin.site.register(Post, PostAdmin) 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+from .models import Post
+
+
+def index(request):
+# одна строка вместо тысячи слов на SQL
+    latest = Post.objects.order_by('-pub_date')[:11]
+    return render(request, "index.html", {"posts": latest})
+    # собираем тексты постов в один, разделяя новой строкой
+    output = []
+    for item in latest:
+        output.append(item.text)
+    return HttpResponse('\n'.join(output))
