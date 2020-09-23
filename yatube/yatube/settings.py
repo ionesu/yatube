c3d4b1799fbb5413f688cefc5bad858d5794cf6b
Ivan Sushkov <ivan.sushkov91@gmail.com>
2020-09-23 20:41:26 +0300

    Add: Static

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -119,7 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# задаём произвольный URL, который будет использоваться для запросов к статическим файлам
+STATIC_URL = "/static/" 
+# теперь логотип можно будет запросить по адресу sitename.ex**/static/**images/logo.png
+
+# задаём адрес директории, куда командой *collectstatic* будет собрана вся статика
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
